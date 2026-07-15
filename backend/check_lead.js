5e7b4bb01b0d7b12aa78a63847e80dd2a0780a20
Mohd Shahil <sahilsgf26@gmail.com>
2026-07-15 17:34:08 +0530

    clean up temporary testing scripts

--- backend/check_lead.js
+++
@@ -1,8 +0,0 @@
-import db from './src/config/db.js';
-
-async function checkLead() {
-  const result = await db.query("SELECT id, l1_details, submitted_by FROM leads WHERE id = 'lead-779896'");
-  console.log(JSON.stringify(result.rows, null, 2));
-  process.exit(0);
-}
-checkLead();
